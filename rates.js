8f9b93f33890c4f636996d39763ab619b8c56b48
sarahlynne96 <158410220+sarahlynne96@users.noreply.github.com>
2025-07-25 14:21:17 -0600

    Update rates.js

--- rates.js
+++ rates.js
@@ -1,1 +1,6 @@
 // rates.js - fetches live SGIP rates from backend
+export async function fetchRates() {
+  const res = await fetch("http://localhost:3000/api/rates");
+  return await res.json();
+}
+// rates.js - fetches live SGIP rates from backend
